55d634d6156891d828204bb132cee4f5301d4a07
Lewis Smith <66628534+PKThunda291@users.noreply.github.com>
2026-09-22 14:01:57 +0100

    Changed import path. all input validation tests pass right now

--- prime.test.js
+++ prime.test.js
@@ -1,6 +1,6 @@
 import assert from 'node:assert/strict'
 import {describe, it} from 'node:test'
-import { generatePrimes } from './prime'
+import { generatePrimes } from './prime.js'
 
 describe("Testing generatePrimes(n) function", () => {
   describe("Input validation", () => {
