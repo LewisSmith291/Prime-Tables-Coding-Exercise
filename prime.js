d785bfe9a6ba92579d38951dd3c60dfd1b43eb45
Lewis Smith <66628534+PKThunda291@users.noreply.github.com>
2026-09-22 14:32:48 +0100

    Finished getUpperBound function for returning the limit the sieve needs for each n input

--- prime.js
+++ prime.js
@@ -1,9 +1,24 @@
-// Given 'n', generates n+1 number of prime numbers, starting from 2
+// Given 'n', generates n number of prime numbers, starting from 2
 export function generatePrimes(n){
   // Input validation
   if (!Number.isInteger(n)) throw new TypeError(`Expected integer parameter, got ${n}`);
   if (n <= 0) throw new RangeError(`parameter must be above 0, got ${n}`);
 
+  const output = [];
+
+  return output;
+}
+
+// This helper function generates an upper bound for the sieve to use
+// Returns a number where the nth prime is guaranteed to be at or below
+// Uses Rosser's theorem: for n >= 6: n(ln n + ln ln n)
+export function getUpperBound(n){
+  // Formula does not work for n < 6
+  if (n < 6){
+    return 12;
+  }
+  // Formula is: n * (log(n) + log(log(n)))
+  // Round up for usable index on array
+  return Math.ceil(n * (Math.log(n) + Math.log(Math.log(n))));
 
 }
-console.log(generatePrimes(4.2));
