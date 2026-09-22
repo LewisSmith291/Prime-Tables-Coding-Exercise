99ec93970c94ba6dc90d2010f36ffac819cd37c7
Lewis Smith <66628534+PKThunda291@users.noreply.github.com>
2026-09-22 14:38:56 +0100

    Added tests for getUpperBound fuinction, all pass. Didnt include input validation as cli input will filter n

--- prime.test.js
+++ prime.test.js
@@ -1,6 +1,6 @@
 import assert from 'node:assert/strict'
 import {describe, it} from 'node:test'
-import { generatePrimes } from './prime.js'
+import { generatePrimes, getUpperBound } from './prime.js'
 
 describe("Testing generatePrimes(n) function", () => {
   describe("Input validation", () => {
@@ -14,5 +14,21 @@ describe("Testing generatePrimes(n) function", () => {
       assert.throws(() => generatePrimes(0), RangeError);
     })
 
+  })
+})
+
+describe("Testing getUpperBound(n) function", () => {
+  it("n < 6 returns 12", () => {
+    assert.strictEqual(getUpperBound(1), 12);
+    assert.strictEqual(getUpperBound(3), 12);
+    assert.strictEqual(getUpperBound(5), 12);
+    assert.strictEqual(getUpperBound(0), 12);
+    assert.strictEqual(getUpperBound(-10), 12);
+  })
+  it("n >= 6 returns appropriate limit", () => {
+    const knownPrimes = [[6,13], [10,29], [100, 541], [1000, 7919]];
+    for (const [n, nthPrime] of knownPrimes){
+      assert.ok(getUpperBound(n) >= nthPrime, `upper bound for n=${n} should be >= ${nthPrime}`);
+    }
   })
 })
