5d9e47e5ab730d3e5d13588e9b9b9479aba232bf
Lewis Smith <66628534+PKThunda291@users.noreply.github.com>
2026-09-22 14:03:59 +0100

    Added decimal input test.

--- prime.js
+++ prime.js
@@ -1,5 +1,9 @@
+// Given 'n', generates n+1 number of prime numbers, starting from 2
 export function generatePrimes(n){
   // Input validation
   if (!Number.isInteger(n)) throw new TypeError(`Expected integer parameter, got ${n}`);
-  if (n <= 0) throw new RangeError(`parameter must be above 0, got ${n}`)
+  if (n <= 0) throw new RangeError(`parameter must be above 0, got ${n}`);
+
+
 }
+console.log(generatePrimes(4.2));
--- prime.test.js
+++ prime.test.js
@@ -4,9 +4,10 @@ import { generatePrimes } from './prime.js'
 
 describe("Testing generatePrimes(n) function", () => {
   describe("Input validation", () => {
-    it("non-number n throws TypeError", () => {
+    it("non whole number n throws TypeError", () => {
       assert.throws(() => generatePrimes("a"), TypeError);
       assert.throws(() => generatePrimes(true), TypeError);
+      assert.throws(() => generatePrimes(4.5), TypeError);
     })
     it("n <= 0 throws RangeError", () => {
       assert.throws(() => generatePrimes(-5), RangeError);
